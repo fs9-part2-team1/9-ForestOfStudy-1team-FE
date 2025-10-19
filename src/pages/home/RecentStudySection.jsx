6f90a1d1db0f2dcbe4aa22ebb309b00def138925
something-like <aqua.psh@gmail.com>
2025-10-19 12:12:43 +0900

    :sparkles: feat: 홈 기능 추가

---
+++ src/pages/home/RecentStudySection.jsx
@@ -0,0 +1,13 @@
+import { useEffect } from 'react';
+import styles from './HomePage.module.css';
+
+function RecentStudySection() {
+  return (
+    <div className="recentStudyContainer">
+      <h1 className={styles.sectionTitle}>최근 조회한 스터디</h1>
+      <div className="RecentStudyCardSection"></div>
+    </div>
+  );
+}
+
+export default RecentStudySection;
