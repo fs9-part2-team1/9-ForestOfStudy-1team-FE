e254312d9b65dc2985c30f66cb1f72ea99506833
yoorrll <lyl1567@naver.com>
2025-10-20 09:49:54 +0900

    :recycle: refactor: 누락 사항

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import { HomePage } from '@/pages/home';
 import { MakeStudyPage } from '@/pages/make-study';
 import { StudyDetailPage } from '@/pages/study-detail';
@@ -7,14 +7,12 @@ import { TodayHabitPage } from '@/pages/today-habit';
 
 export default function AppRoutes() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/make-study" element={<MakeStudyPage />} />
-        <Route path="/study-detail" element={<StudyDetailPage />} />
-        <Route path="/today-focus" element={<TodayFocusPage />} />
-        <Route path="/today-habit" element={<TodayHabitPage />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/make-study" element={<MakeStudyPage />} />
+      <Route path="/study-detail" element={<StudyDetailPage />} />
+      <Route path="/today-focus" element={<TodayFocusPage />} />
+      <Route path="/today-habit" element={<TodayHabitPage />} />
+    </Routes>
   );
 }
