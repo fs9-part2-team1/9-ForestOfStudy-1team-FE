ba02662a997bbdd857b34fed972df515c8b1bfb8
yoorrll <lyl1567@naver.com>
2025-11-07 16:29:01 +0900

    배포 관련 수정사항

--- src/api/client.js
+++ src/api/client.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-  // baseURL: 'import.meta.env.VITE_API_BASE_URL',
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL || '/api',
   headers: {
     'Content-Type': 'application/json',
   },
